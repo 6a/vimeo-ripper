66ccefda16ee62d5ca99b61452041fd92ea3b35d
james_stanton <james.stanton@sony.com>
2018-11-27 18:58:40 +0900

    Trying to add quality option

--- src/lambda/get.js
+++ src/lambda/get.js
@@ -2,20 +2,34 @@ const axios = require('axios');
 exports.handler = (event, context, callback) => {
   const id = event.queryStringParameters.id;
   const url = `https://player.vimeo.com/video/${id}/config`;
-
+  const quality = event.queryStringParameters.quality;
   axios.get(url)
     .then((res) => {
       var bestRes = 0;
       var bestWidth = 0;
       var videoUrl = "";
-      for (var index = 0; index < res.data.request.files.progressive.length; index++) {
-        if (res.data.request.files.progressive[index].height > bestRes) {
-          videoUrl = res.data.request.files.progressive[index].url;
-          bestRes = res.data.request.files.progressive[index].height;
-          bestWidth = res.data.request.files.progressive[index].width;
+
+      if (quality && [360, 540, 720, 1080].includes(quality)) {
+        for (var index = 0; index < res.data.request.files.progressive.length; index++) {
+          if (res.data.request.files.progressive[index].height == quality) {
+            videoUrl = res.data.request.files.progressive[index].url;
+            bestRes = res.data.request.files.progressive[index].height;
+            bestWidth = res.data.request.files.progressive[index].width;
+            break;
+          }
+        }
+      } 
+      
+      if (!quality || videoUrl == "") {
+        for (var index = 0; index < res.data.request.files.progressive.length; index++) {
+          if (res.data.request.files.progressive[index].height > bestRes) {
+            videoUrl = res.data.request.files.progressive[index].url;
+            bestRes = res.data.request.files.progressive[index].height;
+            bestWidth = res.data.request.files.progressive[index].width;
+          }
         }
       }
-      
+     
       var posterURL = res.data.video.thumbs[bestWidth];
 
       callback(null, {
