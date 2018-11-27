7ddb711c6edea8bf3c1c95b6f3b7753c1b0f5c46
James Stanton <jestanton@outlook.com>
2018-11-28 00:17:51 +0900

    Why is the parameter not working??

--- src/lambda/get.js
+++ src/lambda/get.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 exports.handler = (event, context, callback) => {
   const id = event.queryStringParameters.id;
   const url = `https://player.vimeo.com/video/${id}/config`;
-  console.log(event.queryStringParameters.q);
+  console.log(event.queryStringParameters);
   const quality = parseInt(event.queryStringParameters.q);
   axios.get(url)
     .then((res) => {
