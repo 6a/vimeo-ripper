78e3aa890ce29aa2905f147a126c29677e1fc969
james_stanton <james.stanton@sony.com>
2018-11-27 17:54:53 +0900

    Added debug prin

--- src/lambda/get.js
+++ src/lambda/get.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 exports.handler = async (event, context, callback) => {
     const id = event.queryStringParameters.id || "";
     const url = "https://player.vimeo.com/video/" + id + "/config";
-
+    console.log(url);
     axios.get(url)
     .then((res) => {
       callback(null, {
