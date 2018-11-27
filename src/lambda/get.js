83b0622efda2225398742ad72320ced28ff16f93
james_stanton <james.stanton@sony.com>
2018-11-27 18:05:46 +0900

    Trying to get the url build working

--- src/lambda/get.js
+++ src/lambda/get.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 exports.handler = (event, context, callback) => {
   const id = event.queryStringParameters.id || "";
   const url = "https://player.vimeo.com/video/" + id + "/config";
+  console.log(url);
   axios.get(url)
     .then((res) => {
       callback(null, {
