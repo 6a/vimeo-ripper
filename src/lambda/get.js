d042d16ae5858f79202fd08b49e0f31f64dad4ff
James Stanton <jestanton@outlook.com>
2018-11-28 00:15:35 +0900

    why has this broken again

--- src/lambda/get.js
+++ src/lambda/get.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 exports.handler = (event, context, callback) => {
   const id = event.queryStringParameters.id;
   const url = `https://player.vimeo.com/video/${id}/config`;
+  console.log(event.queryStringParameters.q);
   const quality = parseInt(event.queryStringParameters.q);
   axios.get(url)
     .then((res) => {
       var bestRes = 0;
       var bestWidth = 0;
       var videoUrl = "";
-      console.log(quality);
       if (quality && [640, 960, 1280, 1920].includes(quality)) {
         for (var index = 0; index < res.data.request.files.progressive.length; index++) {
           if (res.data.request.files.progressive[index].width == quality) {
@@ -18,7 +18,7 @@ exports.handler = (event, context, callback) => {
             bestWidth = res.data.request.files.progressive[index].width;
             break;
           }
-        }
+        } 
       }
 
       if (!quality || videoUrl == "") {
@@ -33,7 +33,7 @@ exports.handler = (event, context, callback) => {
 
       var posterURL = res.data.video.thumbs[quality] || res.data.video.thumbs[bestWidth] || res.data.video.thumbs[1920]
       || res.data.video.thumbs[1280] || res.data.video.thumbs[960] || res.data.video.thumbs[640];
-      console.log(bestWidth);
+
       callback(null, {
         statusCode: 200,
         body: `{"video": "${videoUrl}", "poster": "${posterURL}"}`,
