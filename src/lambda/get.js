9d505cbe47b2bb7a149987dffaad0f6e6be76a36
james_stanton <james.stanton@sony.com>
2018-11-27 18:33:32 +0900

    So its not broken maybe my code just sucks?

--- src/lambda/get.js
+++ src/lambda/get.js
@@ -2,10 +2,10 @@ const axios = require('axios');
 exports.handler = (event, context, callback) => {
   const id = event.queryStringParameters.id;
   const url = `https://player.vimeo.com/video/${id}/config`;
-  console.log(url);
+
   axios.get(url)
     .then((res) => {
-      console.log(res);
+      console.log(res.request.files);
       callback(null, {
         statusCode: 200,
         body: "HELLO DARKNESS MY OLD FRIEND"
