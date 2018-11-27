a00469bb54190ca4a22bb33088c0fb17aaf86615
james_stanton <james.stanton@sony.com>
2018-11-27 17:59:53 +0900

    Trying non callback method

--- src/lambda/get.js
+++ src/lambda/get.js
@@ -1,19 +1,26 @@
 const axios = require('axios');
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event, context) => {
     const id = event.queryStringParameters.id || "";
     const url = "https://player.vimeo.com/video/" + id + "/config";
-    console.log(url);
+
     axios.get(url)
-    .then((res) => {
-      callback(null, {
-        statusCode: 200,
-        body: res.data,
-      });
+    .then(response => response.json())
+    .then(function(data) {
+        var bestRes = 0;
+        var videoUrl = "";
+        for (var index = 0; index < data.request.files.progressive.length; index++) {
+            if (data.request.files.progressive[index].height > bestRes) {
+                videoUrl = data.request.files.progressive[index].url;
+            }
+        }
+        var data = {
+            statusCode: 200,
+            url: {url: videoUrl}
+        }
+        return data
     })
-    .catch((err) => {
-      callback(err);
-    });
+    .catch(error => ({ statusCode: 422, body: String(error) }));
 
     // return fetch(url)
     //     .then(response => response.json())
