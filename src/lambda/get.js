ed9ee18cbea47ecd5285d3cb6fd659e1049afc2a
james_stanton <james.stanton@sony.com>
2018-11-27 18:27:57 +0900

    So the body wasnt being parsed properly

--- src/lambda/get.js
+++ src/lambda/get.js
@@ -1,14 +1,16 @@
 const axios = require('axios');
 exports.handler = (event, context, callback) => {
-
-  const url = "https://player.vimeo.com/video/291712532/config";
+  const id = event.queryStringParameters.id;
+  const url = `https://player.vimeo.com/video/${id}/config`;
   console.log(url);
   axios.get(url)
     .then((res) => {
       callback(null, {
         statusCode: 200,
-        body: "TEST",
-      });
+        body: {
+          "video-url": "",
+          "poster-url": ""
+      }});
     })
     .catch((err) => {
       callback(err);
