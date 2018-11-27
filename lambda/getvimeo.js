d82b3a19b717ea4f4ed86aaa8f2d4b20b29f0452
james_stanton <james.stanton@sony.com>
2018-11-27 16:52:58 +0900

    Added url getting logic attempt #1

--- lambda/getvimeo.js
+++ lambda/getvimeo.js
@@ -1,8 +1,25 @@
+import fetch from "node-fetch";
+
 exports.handler = async (event, context) => {
     const id = event.queryStringParameters.id || "";
-  
-    return {
-      statusCode: 200,
-      body: `VIMEO ID: ${id}`
-    };
-  };
+    const url = "https://player.vimeo.com/video/" + id + "/config";
+
+    
+    return fetch(url)
+        .then(response => response.json())
+        .then(function(data) {
+            var bestRes = 0;
+            var videoUrl = "";
+            for (var index = 0; index < data.request.files.progressive.length; index++) {
+                if (data.request.files.progressive[index].height > bestRes) {
+                    videoUrl = data.request.files.progressive[index].url;
+                }
+            }
+            var data = {
+                statusCode: 200,
+                url: videoUrl
+            }
+            return data
+        })
+        .catch(error => ({ statusCode: 422, body: String(error) }));
+};
